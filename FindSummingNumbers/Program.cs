5ee840bc5ba63f941239bc91de65a8d16430b681
visualstudio <visualstudio@122-PC04>
2019-04-17 09:31:19 -0400

    FindSummingNumbers

--- FindSummingNumbers/Program.cs
+++ FindSummingNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,50 @@ namespace FindSummingNumbers
     {
         static void Main(string[] args)
         {
+            new GeneralList().Run(57);
+        }
+
+
+        class GeneralList
+        {
+            const int LEN = 100;
+            public ArrayList ListQ;
+            int AddUpToNumber = 0;
+            public void Run(int AddUpToNumber)
+
+            {
+                Random r = new Random();
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                }
+                this.findTwoNumbersThatAddUpTo();
+            }
+
+            public void findTwoNumbersThatAddUpTo()
+            {
+                foreach (int num1 in ListQ)
+                {
+                    int a, b;
+                    a = num1;
+                    foreach (int num2 in ListQ)
+                    {
+                        b = num2;
+                        if(a+b == 57)
+                        {
+                            Console.WriteLine("The Perfect match is found. Numbers are: " + a + " " + b);
+                        }   
+                    }
+
+
+                }
+                Console.ReadLine();
+            }
+
+
+            
+
         }
     }
 }
